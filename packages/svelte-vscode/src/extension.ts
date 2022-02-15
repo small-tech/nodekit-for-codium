124f04ddf71de1084fccc8781a95ea8d5903cef0
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2022-02-15 10:49:13 +0000

    Fix path to language server

--- packages/svelte-vscode/src/extension.ts
+++ packages/svelte-vscode/src/extension.ts
@@ -54,7 +54,7 @@ export function activate(context: ExtensionContext) {
                 : path.join(rootPath as string, tempLsPath)
             : undefined;
 
-    const serverModule = require.resolve(lsPath || 'nodekit-language-server/bin/server.js');
+    const serverModule = require.resolve(lsPath || '@small-tech/nodekit-language-server/bin/server.js');
     console.log('Loading server from ', serverModule);
 
     // Add --experimental-modules flag for people using node 12 < version < 12.17
